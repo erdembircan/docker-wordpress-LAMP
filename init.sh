f59521931d2608a2b360abc21552ed5a11842392
Erdem Bircan <erdem.erdembircan@gmail.com>
2022-08-11 10:49:47 +0300

    mac update for sh file

--- init.sh
+++ init.sh
@@ -17,12 +17,12 @@ project_prefix=${project_name//[ ]/-}
 cp docker-compose_template.yml docker-compose.yml
 
 #rewrite the file with user arguments
-sed -i "s/{server_port}/$server_port/g" docker-compose.yml
-sed -i "s/{project_prefix}/$project_prefix/g" docker-compose.yml
-sed -i "s/{db_password}/$db_password/g" docker-compose.yml
-sed -i "s/{dbadmin_port}/$dbadmin_port/g" docker-compose.yml
-sed -i "s/{db_name}/$db_name/g" docker-compose.yml
-sed -i "s/{db_prefix}/$db_prefix/g" docker-compose.yml
+sed -i "" "s/{server_port}/$server_port/g" docker-compose.yml
+sed -i "" "s/{project_prefix}/$project_prefix/g" docker-compose.yml
+sed -i "" "s/{db_password}/$db_password/g" docker-compose.yml
+sed -i "" "s/{dbadmin_port}/$dbadmin_port/g" docker-compose.yml
+sed -i "" "s/{db_name}/$db_name/g" docker-compose.yml
+sed -i "" "s/{db_prefix}/$db_prefix/g" docker-compose.yml
 
 #clean up
 rm -rf .git .gitignore init.sh docker-compose_template.yml LICENSE README.md
